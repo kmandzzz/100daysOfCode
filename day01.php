01662712ca05255f5f632733ac52be85d153d6c3
Amanda Oliveira <amandadias8123@gmail.com>
2021-11-09 21:53:42 -0300

    Day 01 of learning PHP

---
+++ day01.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+  <title>Day 01 | PHP</title>
+
+  <h3>100 DAYS OF CODE</h3>
+  <?php
+
+  $variabelTest = "Hello World";
+  echo 'Let\'s start with a simple variable: ' . $variabelTest . '<br>';
+
+  // This is a comment
+  /* This is a comment too */
+
+  $string = "Hello World!";
+  $int = 1;
+  $null = null;
+  $boolean = true;
+  $array = [1, 2, 3];
+
+  ?>
+
+</html>
